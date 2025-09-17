1e604d4bcd912530134f9a6efc7b5b2bb494dcae
erle-jas <jasonerlem@oru.edu>
2025-09-17 09:19:57 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+// Define variables
+let myName = "Jason Erlemeier";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Resident Advisor, Teacher's Assistant, and Internship at 111 Consulting";
+let skills = ["Drumming", "Golfing", "Not Coding"];
+//let skills = "something"; //TODO
+
+// Connect variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
