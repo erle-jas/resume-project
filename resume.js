12246359b29779cae68e750afd38f1f5653e18f9
erle-jas <jasonerlem@oru.edu>
2025-09-23 15:16:08 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,10 +1,13 @@
 // Define variables
 let myName = "Jason Erlemeier";
+let MyEmail = "jasonerlem@oru.edu";
 let title = "Student";
 let education = "Oral Roberts University";
 let experience = "Resident Advisor, Teacher's Assistant, and Internship at 111 Consulting";
-let skills = ["Drumming", "Golfing", "Not Coding"];
-//let skills = "something"; //TODO
+let skills = ["Drumming", "Golfing", "Financial Analysis"];
+function showFunFact() {
+ alert("Fun Fact: I love basketball!");
+}
 
 // Connect variables to the HTML (ligaments)
 document.getElementById("my-name").textContent = myName;
